884d21289571ef0db3c968883824c5bfcc61eb28
KanagaPriya <kanagapriyakpr@gmail.com>
2016-07-09 09:41:32 -0700

    Create w2interval.java

---
+++ displayevennob/w2interval.java
@@ -0,0 +1,30 @@
+package sample;
+import java.util.*;
+
+public class evenno {
+
+ evenno(int a,int b)
+{
+	int i;
+	a++;
+	System.out.println("the odd number betwwen two intervals "+a+ "and"+b+ "is");
+	for(i=a;i<b;i++)
+	{
+	if(i%2==0)
+		{
+			System.out.println(i);
+		}
+	}
+}
+public static void main(String args[])
+{
+Scanner	sc=new Scanner(System.in);
+int c,d;
+System.out.println("Enter the starting and ending number");
+c=sc.nextInt();
+d=sc.nextInt();
+evenno ob=new evenno(c,d);
+
+}
+
+}
